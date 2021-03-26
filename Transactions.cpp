7ba72068e37830f4ce20a8f937f0304e31b27416
Tushar Lalwani <79745913+tushar1lalwani1926@users.noreply.github.com>
2021-03-26 21:41:10 +0530

    Add files via upload

---
+++ Transactions.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include "Transactions.h"
+
+using namespace std;
+
+Transaction::Transaction(int amt, string kind) :amount(amt), type(kind){
+}
+
+string Transaction::Report(){
+    string report;
+    report += "   ";
+    report += type;
+    report += " ";
+    report += to_string(amount);
+
+    return report;
+}
